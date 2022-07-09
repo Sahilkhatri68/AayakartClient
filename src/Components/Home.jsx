b32d1e51e8e15e2bb6123b10506ceb5c7c5b5821
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2022-07-09 11:46:09 +0530

    minor

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,67 +1,61 @@
-import { Box, makeStyles } from '@material-ui/core';
-import NavBar from './Home/NarBar';
-import Banner from './Home/Banner';
-import MidSlide from './Home/MidSlide';
-import MidSection from './Home/MidSection';
-import Slide from './Home/Slide';
-import React,  { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux'; // hooks
-import { getProducts as listProducts } from '../redux/actions/productActions';
-
+import { Box, makeStyles } from "@material-ui/core";
+import NavBar from "./Home/NarBar";
+import Banner from "./Home/Banner";
+import MidSlide from "./Home/MidSlide";
+import MidSection from "./Home/MidSection";
+import Slide from "./Home/Slide";
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux"; // hooks
+import { getProducts as listProducts } from "../redux/actions/productActions";
 
 const useStyle = makeStyles({
-    component: {
-        padding: 10,
-        background: '#F2F2F2'
-    }
-})
+  component: {
+    padding: 10,
+    background: "#F2F2F2",
+  },
+});
 
 const Home = () => {
-    const classes = useStyle();
+  const classes = useStyle();
 
-    const getProducts = useSelector(state => state.getProducts);
-    const { products} = getProducts;
+  const getProducts = useSelector((state) => state.getProducts);
+  const { products } = getProducts;
 
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
+  useEffect(() => {
+    dispatch(listProducts());
+  }, [dispatch]);
 
-    return (
-        <> 
-            <NavBar />
-            <Box className={classes.component}>
-                <Banner />
-                <MidSlide products={products} />
-                <MidSection />
-                <Slide
-                    data={products} 
-                    title='Discounts for You'
-                    timer={false} 
-                    multi={true} 
-                />
-                <Slide
-                    data={products} 
-                    title='Suggested Items'
-                    timer={false} 
-                    multi={true} 
-                />
-                <Slide
-                    data={products} 
-                    title='Top Selection'
-                    timer={false} 
-                    multi={true} 
-                />
-                <Slide
-                    data={products} 
-                    title='Recommended Items'
-                    timer={false} 
-                    multi={true} 
-                />
-            </Box>
-        </>
-    )
-}
+  return (
+    <>
+      <NavBar />
+      <Box className={classes.component}>
+        <Banner />
+        <MidSlide products={products} />
+        <MidSection />
+        <Slide
+          data={products}
+          title="Discounts for You"
+          timer={false}
+          multi={true}
+        />
+        <Slide
+          data={products}
+          title="Suggested Items"
+          timer={false}
+          multi={true}
+        />
+        <Slide
+          data={products}
+          title="Top Selection"
+          timer={false}
+          multi={true}
+        />
+        <Slide data={products} title="Recommended" timer={false} multi={true} />
+      </Box>
+    </>
+  );
+};
 
 export default Home;
