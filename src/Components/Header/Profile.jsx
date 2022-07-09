076e67cb046137abb8df6d58b49bc87976311d76
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2022-07-09 17:32:58 +0530

    profile

--- src/Components/Header/Profile.jsx
+++ src/Components/Header/Profile.jsx
@@ -3,53 +3,41 @@ import { Link } from "react-router-dom";
 import { Typography, Menu, MenuItem, makeStyles } from "@material-ui/core";
 import { PowerSettingsNew } from "@material-ui/icons";
 
-const useStyle = makeStyles({
-  component: {
-    marginTop: 40,
-  },
-  logout: {
-    fontSize: 14,
-    marginLeft: 20,
-  },
-});
+import "./profile.css";
 
 export default function Profile() {
-  const [open, setOpen] = useState(false);
-  const classes = useStyle();
-
-  const handleClick = (event) => {
-    setOpen(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
-  //   const logout = () => {
-  //     setAccount("");
-  //   };
-
   return (
     <>
-      <Link onClick={handleClick}>
-        {/* <Typography style={{ marginTop: 2 }}>{account}</Typography> */}
-      </Link>
-      <Menu
-        anchorEl={open}
-        open={Boolean(open)}
-        onClose={handleClose}
-        className={classes.component}
-      >
-        <MenuItem
-          onClick={() => {
-            handleClose();
-            // logout();
-          }}
-        >
-          <PowerSettingsNew color="primary" />
-          <Typography className={classes.logout}>Logout</Typography>
-        </MenuItem>
-      </Menu>
+      <div>
+        <h2 style={{ textAlign: "center" }}>User Profile Card</h2>
+        <div className="card">
+          <img
+            src="https://www.w3schools.com/w3images/team2.jpg"
+            alt="John"
+            style={{ width: "100%" }}
+          />
+          <h1>John Doe</h1>
+          <p className="title">CEO &amp; Founder, Example</p>
+          <p>Harvard University</p>
+          <div style={{ margin: "24px 0" }}>
+            <a href="#">
+              <i className="fa fa-dribbble" />
+            </a>
+            <a href="#">
+              <i className="fa fa-twitter" />
+            </a>
+            <a href="#">
+              <i className="fa fa-linkedin" />
+            </a>
+            <a href="#">
+              <i className="fa fa-facebook" />
+            </a>
+          </div>
+          <p>
+            <button>Contact</button>
+          </p>
+        </div>
+      </div>
     </>
   );
 }
