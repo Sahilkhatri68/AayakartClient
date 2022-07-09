30aa13b76608ce50fd79ccf2e85391b08e0efc8a
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2022-07-09 16:23:35 +0530

    search fun

--- src/Components/Header/Search.jsx
+++ src/Components/Header/Search.jsx
@@ -1,76 +1,92 @@
-import { useState, useEffect } from 'react';
-import SearchIcon from '@material-ui/icons/Search';
-import { makeStyles, InputBase, List, ListItem } from '@material-ui/core';
-import { useSelector, useDispatch } from 'react-redux'; // hooks
-import { getProducts as listProducts } from '../../redux/actions/productActions';
-import { Link } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import SearchIcon from "@material-ui/icons/Search";
+import { makeStyles, InputBase, List, ListItem } from "@material-ui/core";
+import { useSelector, useDispatch } from "react-redux"; // hooks
+import { getProducts as listProducts } from "../../redux/actions/productActions";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
-const useStyle = makeStyles(theme => ({
-    search: {
-        borderRadius: 2,
-        marginLeft: 10,
-        width: '38%',
-        backgroundColor: '#fff',
-        display: 'flex'
-      },
-      searchIcon: {
-        marginLeft: 'auto',
-        padding: 5,
-        display: 'flex',
-        color: 'blue'
-      },
-      inputRoot: {
-        fontSize: 'unset',
-        width: '100%'
-      },
-      inputInput: {
-        paddingLeft: 20,
-        width: '100%',
-    },
-    list: {
-      position: 'absolute',
-      color: '#000',
-      background: '#FFFFFF',
-      marginTop: 36
-    }
-}))
+const useStyle = makeStyles((theme) => ({
+  search: {
+    borderRadius: 2,
+    marginLeft: 10,
+    width: "38%",
+    backgroundColor: "#fff",
+    display: "flex",
+  },
+  searchIcon: {
+    marginLeft: "auto",
+    padding: 5,
+    display: "flex",
+    color: "blue",
+  },
+  inputRoot: {
+    fontSize: "unset",
+    width: "100%",
+  },
+  inputInput: {
+    paddingLeft: 20,
+    width: "100%",
+  },
+  list: {
+    position: "absolute",
+    color: "#000",
+    background: "#FFFFFF",
+    marginTop: 36,
+  },
+}));
 
-const Search = () => {
-    const classes = useStyle();
-    const [ text, setText ] = useState();
-    const [ open, setOpen ] = useState(true)
+export default function Search() {
+  const classes = useStyle();
+  // const [text, setText] = useState();
+  // const [open, setOpen] = useState(true);
 
-    const getText = (text) => {
-        setText(text);
-        setOpen(false)
-    }
+  // const getText = (text) => {
+  //   setText(text);
+  //   setOpen(false);
+  // };
 
-    const getProducts = useSelector(state => state.getProducts);
-    const { products } = getProducts;
+  // const getProducts = useSelector((state) => state.getProducts);
+  // const { products } = getProducts;
 
-    const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(listProducts())
-    }, [dispatch])
+  // useEffect(() => {
+  //   dispatch(listProducts());
+  // }, [dispatch]);
 
-    return (
-        <div className={classes.search}>
-            <InputBase
-              placeholder="Search for products, brands and more"
-              classes={{
-                root: classes.inputRoot,
-                input: classes.inputInput,
-              }}
-              inputProps={{ 'aria-label': 'search' }}
-              onChange={(e) => getText(e.target.value)}
-            />
-            <div className={classes.searchIcon}>
-              <SearchIcon />
-            </div>
-        
-        </div>
-    )
+  const [search, setSearch] = useState("a");
+  const { name } = useParams();
+  async function GetData() {
+    const res = await axios.get(
+      `https://web.chatvait.com/api/products/search/${search}`
+    );
+    console.log(res.data);
+    setSearch(res.data);
+  }
+  useEffect(() => {
+    GetData();
+  }, []);
+
+  const input = (e) => {
+    setSearch(e.target.value);
+    GetData();
+  };
+
+  return (
+    <div className={classes.search}>
+      <InputBase
+        placeholder="Search for products, brands and more"
+        classes={{
+          root: classes.inputRoot,
+          input: classes.inputInput,
+        }}
+        inputProps={{ "aria-label": "search" }}
+        onChange={input}
+      />
+      <div className={classes.searchIcon}>
+        <SearchIcon />
+      </div>
+    </div>
+  );
 }
-
-export default Search;
