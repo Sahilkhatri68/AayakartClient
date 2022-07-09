649ae339c500d38cee5a3ac7a23f8673c0ee7d2c
Sahilkhatri68 <86000524+Sahilkhatri68@users.noreply.github.com>
2022-07-09 15:44:55 +0530

    accordion solved

--- src/Components/Home/Viewall.jsx
+++ src/Components/Home/Viewall.jsx
@@ -12,6 +12,9 @@ import Slider from "@mui/material/Slider";
 import Box from "@mui/material/Box";
 import { ExpandMore } from "@material-ui/icons";
 import { useState } from "react";
+import Grid from "@mui/material/Grid";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
 const price = [
   {
@@ -31,9 +34,6 @@ const price = [
     label: "100",
   },
 ];
-// function valuetext(value) {
-//   return `${value}°C`;
-// }
 
 export default function SimpleAccordion() {
   const [age, setAge] = React.useState("");
@@ -41,44 +41,53 @@ export default function SimpleAccordion() {
   const handleChange = (event) => {
     setAge(event.target.value);
   };
+
+  const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: "center",
+    color: theme.palette.text.secondary,
+  }));
   return (
     <div>
-      <Accordion>
-        <AccordionSummary
-          expandIcon={<ExpandMore />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography>Filter products</Typography>
-        </AccordionSummary>
-        <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
-          <InputLabel id="demo-select-small">Category</InputLabel>
-          <Select
-            labelId="demo-select-small"
-            id="demo-select-small"
-            value={age}
-            label="Category"
-            onChange={handleChange}
-          >
-            <MenuItem value="">
-              <em>None</em>
-            </MenuItem>
-            <MenuItem value={10}>Male</MenuItem>
-            <MenuItem value={20}>Female</MenuItem>
-          </Select>
-        </FormControl>
-        <AccordionDetails>
-          <Box sx={{ width: 300 }}>
-            <Slider
-              aria-label="Custom price"
-              defaultValue={20}
-              step={10}
-              valueLabelDisplay="auto"
-              marks={price}
-            />
-          </Box>
-        </AccordionDetails>
-      </Accordion>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4}>
+            <Item>
+              {" "}
+              <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+                <InputLabel id="demo-select-small">Category</InputLabel>
+                <Select
+                  labelId="demo-select-small"
+                  id="demo-select-small"
+                  value={age}
+                  label="Category"
+                  onChange={handleChange}
+                >
+                  <MenuItem value="">
+                    <em>None</em>
+                  </MenuItem>
+                  <MenuItem value={10}>Male</MenuItem>
+                  <MenuItem value={20}>Female</MenuItem>
+                </Select>
+              </FormControl>
+              <Box sx={{ width: 300 }}>
+                <Slider
+                  aria-label="Custom price"
+                  defaultValue={20}
+                  step={10}
+                  valueLabelDisplay="auto"
+                  marks={price}
+                />
+              </Box>
+            </Item>
+          </Grid>
+          <Grid item xs={8}>
+            <Item>xs=8</Item>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
